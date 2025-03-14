942977829df0edf12f9a200f9089a9fab76aaf64
jdj-design <doc_jones12@yahoo.com>
2025-03-14 13:52:00 -0400

    added

--- knex/migrations/20250314175130_create_pet_type_table.js
+++ knex/migrations/20250314175130_create_pet_type_table.js
@@ -3,11 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.createTable('pet_type', table => {
-    table.increments('id');
-    table.string('name').notNullable();
-    table.timestamps(true, true);
-  });
+  
 };
 
 /**
@@ -15,5 +11,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('pet_type');
+  
 };
